5c4879ba44520c4b483d830ada3bdd5af1fa521e
eleve <eleve@si-PC4>
2018-01-24 16:02:29 +0100

    On a fini l'exo 1

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -1,10 +1,13 @@
 //  Copyright (c) 2017 Antoine Tran Tan
-//
+//Copyright (c) 2017 Alice Verot
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
-
-
-
+double exo1(double rayon)
+{
+	double volume;
+	volume = (4.0/3.0) * 3.14*rayon*rayon*rayon;
+		return volume;
+}
 #endif // MY_HEADER_HPP
 
