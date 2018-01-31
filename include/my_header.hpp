9cb69f1796e40857ce0c5c2aa8d0e79b8dc90375
AliceVerot <alice.verot@sfr.fr>
2018-01-31 14:14:40 +0100

    Nous avons fini l'exo 2

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -1,5 +1,5 @@
 //  Copyright (c) 2017 Antoine Tran Tan
-//Copyright (c) 2017 Alice Verot
+//	Copyright (c) 2017 Alice Verot
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
@@ -8,6 +8,11 @@ double exo1(double rayon)
 	double volume;
 	volume = (4.0/3.0) * 3.14*rayon*rayon*rayon;
 		return volume;
+}
+void exo2(short a, short b)
+{
+	short y;
+	y = a < b && !a;
 }
 #endif // MY_HEADER_HPP
 
--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,14 @@
 //  Copyright (c) 2018 Antoine Tran Tan
-//Copyright (c) 2018 MIlan Milosevic
+//	Copyright (c) 2018 MIlan Milosevic
 
 #include <iostream>
 #include <my_header.hpp>
 
 int main()
 {
+	exo2(5, 10);
+	exo2(0, 10);
+	exo2(20, 10);
 	double r1;
 	r1 = exo1(10.0);
     // Affichage sur un terminal
